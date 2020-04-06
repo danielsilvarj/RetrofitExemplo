ecdddc4248691bf2c3905f66f905ce4d590590bc
danielsilvarj <daniel.cg.rio@gmail.com>
2020-04-06 13:24:51 -0300

    Initial commit

---
+++ app/src/main/java/Interface/JsonPlaceHolderApi.java
@@ -0,0 +1,13 @@
+package Interface;
+
+import java.util.List;
+
+import Model.Posts;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Posts>> getPosts();
+}
---
+++ app/src/main/java/Model/Posts.java
@@ -0,0 +1,44 @@
+package Model;
+
+public class Posts {
+
+    private int userId;
+    private int id;
+    private String tittle;
+    private String body;
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+}
